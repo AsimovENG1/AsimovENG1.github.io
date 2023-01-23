5f7e38bfd9a2a878622f3a0efd0c3cafc8721605
TobyMeehan <toby@tobymeehan.com>
2023-01-23 14:10:32 +0000

    Update homepage banner
    Added styles for banner and moved piazza panic content to component

---
+++ components/PiazzaPanic.tsx
@@ -0,0 +1,43 @@
+function PiazzaPanic() {
+    return (
+        <>
+            <h2>Piazza Panic</h2>
+
+            <p>It seems to be a quiet day at the Piazza: there aren't that many students and staff around
+                this time of the year, so most of the kitchen staff are away on leave - you only have three
+                cooks. Suddenly, a crowd bursts into the restaurant: it's an open day, and they all want to eat
+                at once! Make the most of your kitchen and your staff, and face up to the challenge of
+                serving an impatient and demanding crowd!</p>
+
+            <hr />
+
+            <h3>Downloads</h3>
+
+            <div className='row'>
+
+                <div className='col-md-4'>
+                    <h4>PLAY</h4>
+                    <button className='btn btn-primary'>PiazzaPanic.jar</button>
+                </div>
+
+                <div className='col-md-4'>
+                    <h4>Source Code</h4>
+                    <button className='btn btn-primary'>Github</button>
+                </div>
+
+                <div className='col-md-4'>
+                    <h4>Documents</h4>
+                    <ul>
+                        <li>Req1.pdf</li>
+                        <li>Arch1.pdf</li>
+                        <li>Plan1.pdf</li>
+                        <li>Risk1.pdf</li>
+                        <li>Impl1.pdf</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default PiazzaPanic;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+
 import styles from '@/styles/Home.module.css'
+
+import PiazzaPanic from '@/components/PiazzaPanic'
+
+import banner from '@/public/img/banner.svg'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,49 +18,23 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='container-fluid text-center'>
-        <div>
-          <img src='/asimov-lg.png' />
+
+      <div className='container-fluid text-center p-0'>
+        <div className={styles.banner}>
+          <Image className='py-5' src={banner} alt='Asimov' />
         </div>
 
         <hr />
-
-        <h2>Piazza Panic</h2>
-
-        <p>It seems to be a quiet day at the Piazza: there aren't that many students and staff around
-          this time of the year, so most of the kitchen staff are away on leave - you only have three
-          cooks. Suddenly, a crowd bursts into the restaurant: it's an open day, and they all want to eat
-          at once! Make the most of your kitchen and your staff, and face up to the challenge of
-          serving an impatient and demanding crowd!</p>
-
-        <hr />
-
-        <h3>Downloads</h3>
 
         <div className='row'>
+          <div className='col-md-2'></div>
 
-          <div className='col-md-4'>
-            <h4>PLAY</h4>
-            <button className='btn btn-primary'>PiazzaPanic.jar</button>
+          <div className='col-md-8'>
+            <PiazzaPanic></PiazzaPanic>
           </div>
 
-          <div className='col-md-4'>
-            <h4>Source Code</h4>
-            <button className='btn btn-primary'>Github</button>
-          </div>
-
-          <div className='col-md-4'>
-            <h4>Documents</h4>
-            <ul>
-              <li>Req1.pdf</li>
-              <li>Arch1.pdf</li>
-              <li>Plan1.pdf</li>
-              <li>Risk1.pdf</li>
-              <li>Impl1.pdf</li>
-            </ul>
-          </div>
+          <div className='col-md-2'></div>
         </div>
-
       </div>
     </>
   )
