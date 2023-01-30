15cbb511fd18435a405285bcea6ba1d690a949e3
Toby <toby@tobymeehan.com>
2023-01-30 15:51:17 +0000

    Add architecture figures
    Added additional figures to architecture section

--- pages/architecture.tsx
+++ pages/architecture.tsx
@@ -1,3 +1,5 @@
+import { faFilePdf } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Head from "next/head";
 
 export default function Architecture() {
@@ -6,6 +8,106 @@ export default function Architecture() {
             <Head>
                 <title>Architecture</title>
             </Head>
+
+            <h2>Architecture</h2>
+
+            <div className="card mb-3">
+                <div className="card-body">
+                    <a className="btn btn-primary" href="https://drive.google.com/file/d/1vc-FX5Y0KrgTGWzejgZgJqn7mhTEkh-A/view?usp=sharing">
+                        Download Figure 3
+                        {' '}
+                        <FontAwesomeIcon icon={faFilePdf} />
+                    </a>
+
+                    {' '}
+
+                    <a className="btn btn-primary" href="https://drive.google.com/file/d/1vPtADiHawYGA-0d77L5LxdYtXEr7_vF-/view?usp=sharing">
+                        Download Figure 4
+                        {' '}
+                        <FontAwesomeIcon icon={faFilePdf} />
+                    </a>
+                </div>
+            </div>
+
+            <div className="card mb-3">
+                <div className="card-body">
+                    <h5 className="card-title">Figure 1.1</h5>
+                    <img src="https://www.plantuml.com/plantuml/img/ZPJ1JiCm38RlUOgSaNQ09zWOJ48QsaGdE7kj6L6pf4eSj6hYtUdQhcdS8T5J-pd-xJX_ElS4Zi9HYro3ztAheKPt_X7zzp9uiiSnsLglI5bp5h9vFhK5agVHUos6fI_cuB1GQ2WZQ7EYhhMbCeS9pogd23lj7PueE9I-PL7s7RND5TN_wg0ArAtRMwiHZ2onlkVWWOCF3XOSP1mySV1i2Dq4Sl36mOg3vQs1fdjI_Z4OHjP8579az1M16DgOvWGOszaZa8s55z-WDEoL7iOUr_OndPRAmQoqgZXRVjagoZR7dvUTti8vgDVAarJ39Zshj0fQn-IDAnfVJkRW3cpgocUWeRWaTkwMTxFPO6DnTNXA9zPbIObzqzYuIcVdj4RFRbUu9apr-yX8_NeFKlc19drS1fsGzcQ8Fxg8LDngPUm6aQYsPvBsqo4nHrCqlvbV"
+                        alt="UML Figure 1.1 UML" className="img-fluid" />
+                </div>
+            </div>
+
+            <div className="card mb-3">
+                <div className="card-body">
+                    <h5 className="card-title">Figure 1.2</h5>
+                    <img src="https://www.plantuml.com/plantuml/img/bLVDRk8m4BxxAN9iY_865MfXrLMbHJsaXt8qoHGiWeri1ubbzzrt_9F4JXnWEKJ9VEEPyVnyDYzAKwcRGqr8MLEbiXLbV0MyAKe9m2_aRmlyf0SO2GlGcl6j6W7lN6dPb9e9FbvK0gUIYPMeu48-sG5afispLrdkuFj2yeqGDL2UCVMAfav0SiPrLYBurR-koQnzPHMPBKKjPBQZshmqaimABJ548obsz0Xaziwt4Ye6N1UQbljCcIUPdGIhCWNQkNfeVLFx-KXchM3RgJmwpoWmyNuDljVcsnhTIhh1ZtvtZT82ztiX6VxSHhpeQonQzo8jDAtRrSh9Vp2rGvSS9DNm8IkGwCeWNIojwGdvEWoj07d2R2r5mtKNOBjpa-9NxR41v8t5y2nOMUwNGvqse5WhTD9g-Dh84xm9KRc0ZrWrkeMi569lUb1JqstAnUnxLhAwHgnmc8DwWzRVcppJJGtUqzzWzN8dZiSRgwtEnFe5unDBAy3f4ETt3OV4EhF5g00sqttNPeA7ISPK1DbWdLeg8OjcAYHi_Dx30-e5biOXhe3gkmmPnQkMVY63D2NSPSphNhNtmGtbt6FDQbwrzIaEL8kxZ3dLXBMWD0VaknhBGRfQN7fM-POCAiyNJDcsWYgSp4KZjwOXdCg1xi597f9qPXVI3ROPybUsT2FIxIGarXDbjMbW4e-XbxEQwNDCP44ZXjiDQ3KxrhG45xoFth2KFGAOwTjlXBDUFqX3IVa2tbmk8fdFo9I90gc-nDCaZ0BF7QJ4C8U3LA7wgvJq_DpBvnd3eLIEo1NxRKa-t1d0VjAYJ-umgjXyzvxEwR26qPI4YVBvkD8nPlPIAswrN2knPOYgQ4Vc_u8eQ4shi6kjuEQ6tJpTwSe4Q7xntINBytbmQqd2yVqbgJAwoH3I6tL6WliC8U6NXSs1bX379srgdJyk4r1FumamPjI4Ga3U2JGaq0GSSI8Xvg38yp_pO108QNbw1DZJO2HrH3mIUqeToHtjERcX5GpIj5Ge64tbsCfOvIdFXmHnXvgTy4ZFIiOEso3dOoVTZkOt0i11UKRtcFGqu1EV1atoquWjG1foHI0aFa4i5DU4JsIwwvIuBIVLWkQSr0bRT58fQjH9hS4DqUd5cyNDnnV1j59_sqlZmQrkoWjyc-vAD45B1GxsJH_nHyWq2Q2Zac6DdmQ5V05UuR_1Vm00"
+                        alt="Figure 1.2 UML" className="img-fluid" />
+                </div>
+            </div>
+
+            <div className="card mb-3">
+                <div className="card-body">
+                    <h5 className="card-title">Figure 2.1</h5>
+                    <img src="https://www.plantuml.com/plantuml/img/VP6nZiCW44HxlcAxLFa15wT4IDUTN2J-00HZ6za6Xrroz_a78OsBkmuDRsTcjHSMaoHlg-B5Xzqain7ZaH4ili1i9bnNFmMo28AaZ9MOQ60aCao3AXFYhTzD4D8pnghgWzedr-3-26UMkBKVdPNET3v_qD3H7J-I4yZE9anWaaWyn-_cemOgbEvQWr8uYz9LwhiQRnVouplHXod1-LARovjkOkoY-bTAcMyCGwg_f8ngSommbaNVvPs3gs0p-Qk3pXtTa9w5_OpH7Tt_s-yKNp4sRliAmKbT45ovoIy0"
+                        alt="Figure 2.1 UML" className="img-fluid" />
+                </div>
+            </div>
+
+            <div className="card mb-3">
+                <div className="card-body">
+                    <h5 className="card-title">Figure 2.2</h5>
+                    <img src="https://www.plantuml.com/plantuml/img/dP6nZi9034Jx_GftgVY150YqzLKX7-1RdCHYyURMNWH_poOHpPrq4hIZcNaU-Q26sSerEhsmp9Zn2ae_XIJG5wdYICV8eq0WCSeEWwKClL86LEXTJHW7db4C_4J3elfVQffdbh4pD4wo6ZhNmsuFljsQAhYOywlKjN2y8KVyviZsgCSOM881evBp7UoggM_XH7ShcI0DVm1XGXb9brJ30aMf-HUtvkdy8bak7u32LjAyFUmp76vDiKuxa9ph_vu0"
+                        alt="Figure 2.2 UML" className="img-fluid" />
+                </div>
+            </div>
+
+            <div className="card mb-3">
+                <div className="card-body">
+                    <h5 className="card-title">Figure 2.3</h5>
+                    <img src="https://www.plantuml.com/plantuml/img/dP71Ri9034Jl-nM-vSGFv5218ZMddj9yWDaumM9nKjkBuE_PW7geAY71TJGpRsHlpL4zdrAm8yk2YYSm-iaaaRx832VQ9Pu48ecJ1em-A_H62cZGXv9mZhoWE3G76bUr-QEsoYcnJ9sZyooheUr23viFQEf7Km5d3yqgjLqDkpDomZqdzciPu-0pZ9YCGjl1ffZw6hxfuba9vl4V81vG9h8rLR50DgfU3ohXSBs34o_LqxcVnR7GyBlKDRynT5IceNhvBE_1yD6Koc6s94Fv_Wq0"
+                        alt="Figure 2.3 UML" className="img-fluid" />
+                </div>
+            </div>
+
+            <div className="card mb-3">
+                <div className="card-body">
+                    <h5 className="card-title">Figure 2.4</h5>
+                    <img src="https://plantuml.io/plantuml/img/XOyn4eCm30Hxlq8Vo0SeCiIkKgHoU8362F20PIBBcJm_PgXIfJtThUxweYXQqsRA6db7mGI5NfKuq8DAmPbkMvmP0h6I60oQ1OP20bXWC2sXCSGTMS4jD1sg-r5jPA-eCVDnizuCSBc2wqv8-rdLk4EoleEdO5bQ0GNDe5B9M0-Nqt_dMMYChGS8fVoc4IR92Hezh0UYsOQ6e8zM8SZJdqHFFBRXNm00"
+                        alt="Figure 2.4 UML" className="img-fluid" />
+                </div>
+            </div>
+
+            <div className="card mb-3">
+                <div className="card-body">
+                    <h5 className="card-title">Figure 2.5</h5>
+                    <img src="https://www.plantuml.com/plantuml/img/LOyn4eCm30Hxlq9TAZv0aO5ndIv-W6A4qSJ8n9OpoUzZe4gxkjFT3KKnQrsZAK-M3JEkKEXLIJpTg1GCD4OE0fv4AHlqcZAuGXcmW3FDeUnvGr6m2yqxjVyqrTrv799nq5t1zkU75bZLs0Eb7iOtSiG7HzPlAw6W2MQCXOnDq3MHw-5E7wsPm2yeWQPTSc49cIPk1I6recVUnGmaKnls0m00"
+                        alt="Figure 2.5 UML" className="img-fluid" />
+                </div>
+            </div>
+
+            <div className="card mb-3">
+                <div className="card-body">
+                    <h5 className="card-title">Figure 2.6</h5>
+                    <img src="https://www.plantuml.com/plantuml/img/TP0n3i8m34NtdC8Nw0KweA8CA0DJw05CufQeRL8SH-Bu9Ee03AnVx_rlUSY2B6LRLLv2t95nWqpFGj7HbNB6cSvhc2CuYaAiq4bY63CnO8PHLKE22pj60VsWgQNw9pLnPlAXwbPGGegvCSQg4ReJwFuegzj5b6wHiJrS6EyViLua80c42obZeJl4Bm2z9z-Gkk6MrbEHiOSRlQGmmPJQn3zyeEZh2zu0"
+                        alt="Figure 2.6 UML" className="img-fluid" />
+                </div>
+            </div>
+
+            <div className="card mb-3">
+                <div className="card-body">
+                    <h5 className="card-title">Figure 5</h5>
+                    <img src="https://www.plantuml.com/plantuml/img/TLLBZjim3Dth5Bv0K7IMGHVJfErqW0O8wbw0iPXO65aA92g38EtTI_ahovvL8duU7nyf-TaJE0gD5gBIu7rnG1CUujzmV3MUNAX8MTDvr6Jmsw8afynb2Y-HY0s-h1oYUGWqSD9Osc3ajjXPgn6CQ6pmMABnYjHDqNrRA4EYije6jzFAo8ELE8MFu2_Gu03CniYd-F9t4wwRJSvpxfop6dtIlXlsJh138OvrA1KQyeaK7ZH85i3lQtkzebnXgBbGg3198ji0sSHW3LDCpWxU3mZq46Vh6fH7e2Z80laKqgHJS1TqaSkBKrfdN5gYOuC3gTpSCSkjBJtHuK_MdkJ2pgn48jRaQ57NNHs6409EF3AegEXwgceySqUosuQpjR8agDxYZW6fglYbF8aQQD_koRXwR2bhkE9o5oFWvyVy_9IUWsSDeepMINGp00wT6BT0AxQdbIQiVMECbd8SywJ0JzHooV7ixccuLnBt394Eb5GJoVtG5gX8k-4bJ7EZOckRkM0O3BMxGzXqfBuw1_VsV7JecUlA_dbqDwws7x97Hstfg85uvxERieCqh2zRNc5sFP8D6Hk16oWTdutNoJcGZ9dBd6aiYM-yWQgTSVpP5j-OwQS__3UvIyhqt6Upcp1dC_emAfj7X6ixxX-XJC3SEwYuoEgLpEsfd4CZtusCzVi7ClRx6ttGD6WtVp9duK9CZs1MOjMJIf05E7MfgRiVcMSneNM0_dMAKrlryBmokyPphDXVPxwKuXcDZD-q_m00"
+                        alt="Figure 5 UML" className="img-fluid" />
+                </div>
+            </div>
+
+            <div className="card mb-3">
+                <div className="card-body">
+                    <h5 className="card-title">Figure 6</h5>
+                    <img src="https://www.plantuml.com/plantuml/img/jLPDJnin4BtlhvWRn2gDMgaNKY4ALH5I8sZJgfTSZ7UIcEpQAt-0uDTtl5wysNGNjbJb46NmUpFpPiQUd5h7ZVDbaTLVikEV5WqOhonSI_xuoA-vakBjBJUpp625t6NsNZgnWSCQQR50uInm1RfoKYjO6Lt2d4i5SrH-aWbkSGzyi0Ynu8ANU339WFwEE-SBWOeRgM6kSvpj04fzXnR41bSMA8lDKZ7sYx4tZ7sgFnVqIQdamDXdnc2B3tQN7ZDlF81KIoMqSaONuArKQsAz8rRal-_m6-cxgGOttZfTagLDZYOYvGeEppl_1qdbWHjlrcWcu3QeuF01RHJUUXIwYEePcrCi2hzKt3KMKCL2THcR3T4uGS-yQdba3DEgWWkqYQAf5fGPsHT65aKenilK9isMUxxHLVKiLn2YfVxXMOgj1L_Li0hpGU2ga5LAVj2Bqzowsal4GR-GFHZfeQzVjCxXN7lbq0oMO3_DfQgvBvSmvj-l4ejmN_f6oFA2vy-CpLTqpWiCUTG3qDWZPeUnArKZ6cPjZUBzq2LtEX6ZEM9sddYXsZGyuIAFdvudyeZv6OJkTx0LChx_gMolKj5mHuWOGkvhI4ty6mbfQbub8h6RuLMvN6KzOYfwGMEIbsfjC9Ued7rY7L_tdzAWbJTI8LrHAFaMwGwaUt7LFhanr5g6tP3EIOdiFCpn2OVxZInsxfUbbzpSOVxnmoHSilPXFZfHUjVzIYffm-uON4vfkwNbTp1hN4sdtz5vGqkrHfASX9bEP_qhaWfB7R2T1SdpMyexv1mFUyEVSR7zbyYNoZhZHUXxDxf1dbjQc-tnVud_pKj7SMcKuhxEkftR0TF4DEYwTqfFigCJwk0iaDF0PgTaqI-Stm00"
+                        alt="Figure 6 UML" className="img-fluid" />
+                </div>
+            </div>
         </>
     )
 }
